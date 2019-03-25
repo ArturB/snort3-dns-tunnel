a08d7e754b0dac55763ca848ac6f5d354f9278e6
Damian Chilnski <lapsio3@gmail.com>
2019-03-25 18:33:43 +0100

    dns uploader initial version

---
+++ dns_tools/dnsenc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+chunk=24
+domain=$2
+file=$1
+
+if [ ! -f "$file" ] ; then
+	echo "File not found."
+	exit 1
+fi
+
+if [ "$domain" = "" ] ; then
+	echo "Domain not specified."
+	exit 1
+fi
+
+echo "Uploading $file"
+size=$(du -b "$file" | grep -o "^[0-9]\+")
+if [ "$size" = "" ] ; then
+	echo "Error while checking file size"
+	exit 2
+fi
+
+skip=0
+while [ "$((skip*chunk))" -lt "$size" ] ; do
+	sleep 0.4
+	q=$(dd if="$file" skip=$skip bs=$chunk count=1 2>/dev/null | base64)."$domain"
+	#echo "$q"
+	nslookup "$q"
+	skip=$((skip+1))
+done
