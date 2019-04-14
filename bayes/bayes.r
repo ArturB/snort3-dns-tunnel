f770a4da70be4761478d99a66b3f33e8475432c4
Artur Brodzki <artur@brodzki.org>
2019-04-14 02:59:17 +0200

    bayes

--- bayes/bayes.r
+++ bayes/bayes.r
@@ -13,3 +13,19 @@ library(rjson)
 setwd("/Users/artur/Projekty/kant-security/bayes/")
 dnsQueries <- fromJSON(file = "qlog.json")
 dnsFrame <- as.data.frame(dnsQueries)
+print(dnsQueries[[2]][[2]][[1]])
+length(dnsQueries[[1]])
+
+dnsCSV = data.frame( matrix(nrow = length(dnsQueries), ncol = length(dnsQueries[[1]])) )
+rowsCSV = length(dnsQueries)
+colsCSV = length(dnsQueries[[1]])
+for (i in range(1, rowsCSV)) {
+  for(j in range(1,colsCSV)) {
+    dnsCSV[[i,j]] = dnsQueries[[i]][[j]][[2]][[1]]
+  }
+}
+
+dnsDomains = read.csv(file = "qlog.csv", sep = ',', header = FALSE)
+domains = dnsDomains[[2]]
+write.csv(file = "domeny.csv", domains)
+length(domains)
