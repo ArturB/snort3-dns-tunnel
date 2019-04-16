6924b3914b8d6948a7f3e208f30f9d5bc0870732
Artur Brodzki <artur@brodzki.org>
2019-04-16 11:27:38 +0200

    DNS Frquencies binary search array

--- bayes/bayes.r
+++ bayes/bayes.r
@@ -16,7 +16,7 @@ setwd("/Users/artur/Projekty/kant-security/bayes/")
 ####     Poprawne zapytania      ####
 #####################################
 
-validDns.csv <- read.csv(file = "valid-qlog.csv", header = FALSE)
+validDns.csv <- read.csv(file = "valid-queries.csv", header = FALSE)
 validDns.domains = validDns.csv$V2
 
 ######################
@@ -24,9 +24,14 @@ validDns.domains = validDns.csv$V2
 ######################
 
 validDns.lengths = 1:length(validDns.domains)
+validDns.i = 0
+validDns.pb =  txtProgressBar(min = 0, max = length(validDns.domains), initial = 0)
 for(i in 1:length(validDns.domains)) {
   validDns.lengths[[i]] = nchar(toString(validDns.domains[[i]]))
+  validDns.i = validDns.i + 1
+  setTxtProgressBar(validDns.pb, validDns.i)
 }
+close(validDns.pb)
 hist(validDns.lengths, breaks = 63, col = "red", freq = FALSE, ylim=c(0, 0.3), xlim = range(1:40))
 
 #############
@@ -57,23 +62,22 @@ for(ng in names(validDns.ngrams)) {
 validDns.ngrams = validDns.ngrams[order(unlist(validDns.ngrams), decreasing = TRUE)]
 validDns.ngrams = validDns.ngrams[names(validDns.ngrams) != ""]
 validDns.ngrams = validDns.ngrams[nchar(names(validDns.ngrams)) == 3]
-validDns.ngrams.frame = data.frame(
-  ngram = names( validDns.ngrams),
-  prob  = unlist(validDns.ngrams)
-)
-write.csv(x = validDns.ngrams.frame, file = "valid-freqs.csv", quote = FALSE, row.names = FALSE)
 
 #####################################
 ####     Złośliwe zapytania      ####
 #####################################
 
-conn <- file("evil-qlog-small.txt", open = "r")
-evilDns.domains = readLines(conn)
+connHEX <- file("evil-queries-hex.txt", open = "r")
+connB32 <- file("evil-queries-base32.txt", open = "r")
+evilDns.domainsHEX = readLines(connHEX)
+evilDns.domainsB32 = readLines(connB32)
+close(connHEX)
+close(connB32)
 evilDns.ngrams = list()
-evilDns.pb = txtProgressBar(min = 0, max = length(evilDns.domains), initial = 0)
 evilDns.i = 0
 evilDns.nsubs = 0
-for(d in evilDns.domains) {
+evilDns.pb = txtProgressBar(min = 0, max = length(evilDns.domainsHEX) + length(evilDns.domainsB32), initial = 0)
+for(d in c( evilDns.domainsHEX, evilDns.domainsB32 )) {
   for(ng in allSubStrings(d, 3)) {
     if(ng %in% names(evilDns.ngrams)) {
       evilDns.ngrams[[ng]] = evilDns.ngrams[[ng]] + 1.0
@@ -91,11 +95,6 @@ for(ng in names(evilDns.ngrams)) {
   evilDns.ngrams[[ng]] = log( evilDns.ngrams[[ng]] / evilDns.nsubs, base = 2 )
 }
 evilDns.ngrams = evilDns.ngrams[order(unlist(evilDns.ngrams), decreasing = TRUE)]
-evilDns.ngrams.frame = data.frame(
-  ngram = names( evilDns.ngrams),
-  prob  = unlist(evilDns.ngrams)
-)
-write.csv(x = evilDns.ngrams.frame, file = "evil-freqs.csv", quote = FALSE, row.names = FALSE)
 
 #####################################
 ####  Baza prawdopodobieństw     ####
@@ -103,34 +102,45 @@ write.csv(x = evilDns.ngrams.frame, file = "evil-freqs.csv", quote = FALSE, row.
 
 totalDns.names = c( names(validDns.ngrams), names(evilDns.ngrams) )
 totalDns.ngrams = list()
-totalDns.factor = 2
+totalDns.factor = -5
+totalDns.i = 0
+totalDns.pb = txtProgressBar(min = 0, max = length(totalDns.names), initial = 0)
 for(n in totalDns.names) {
   # ONLY-EVIL VASE
   if(! n %in% names(validDns.ngrams)) {
-    totalDns.ngrams[[n]] = -100
+    totalDns.ngrams[[n]] = -20
   }
   # ONLY-VALID CASE
   else if(! n %in% names(evilDns.ngrams)) {
-    totalDns.ngrams[[n]] = 100
+    totalDns.ngrams[[n]] = 20
   }
   # BOTH-VALID CASE
   else {
     totalDns.ngrams[[n]] = validDns.ngrams[[n]] - evilDns.ngrams[[n]] - totalDns.factor + 1
   }
+  totalDns.i = totalDns.i + 1
+  setTxtProgressBar(totalDns.pb, totalDns.i)
 }
-totalDns.ngrams = totalDns.ngrams[order(unlist(totalDns.ngrams), decreasing = TRUE)]
+close(totalDns.pb)
+totalDns.ngrams = totalDns.ngrams[order(names(totalDns.ngrams), decreasing = FALSE)]
 totalDns.ngrams.frame = data.frame(
   ngram = names( totalDns.ngrams),
   prob  = unlist(totalDns.ngrams)
 )
-write.table(x = totalDns.ngrams.frame, file = "dns-freqs.csv", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(
+  x = totalDns.ngrams.frame, 
+  file = "dns.freqs", 
+  quote = FALSE, 
+  row.names = FALSE, 
+  col.names = FALSE
+)
 
 ##################################
 ####  Testy klasyfikatora     ####
 ##################################
 
-isValidDns <- function(d, allNgrams) {
-  if(nchar(d) <= 4) {
+isValidDns <- function(d, allNgrams, lenThr = 5, probThr = 0) {
+  if(nchar(d) <= lenThr) {
     return(TRUE);
   }
   subs <- allSubStrings(d, 3)
@@ -143,11 +153,11 @@ isValidDns <- function(d, allNgrams) {
       prob = prob + allNgrams[[s]]
     }
   }
-  return(prob > 0)
+  return(prob > probThr)
 }
 
-isValidDnsProb <- function(d, allNgrams) {
-  if(nchar(d) <= 4) {
+isValidDnsProb <- function(d, allNgrams, lenThr = 5) {
+  if(nchar(d) <= lenThr) {
     return(100)
   }
   subs <- allSubStrings(d, 3)
@@ -163,21 +173,12 @@ isValidDnsProb <- function(d, allNgrams) {
   return(prob)
 }
 
-isValidDnsDist <- function(d, allNgrams) {
-  if(nchar(d) <= 4) {
-    return(TRUE);
-  }
-  subs <- allSubStrings(d, 3)
-  prob = 0;
-  for(s in subs) {
-    if(! s %in% names(allNgrams)) {
-      return(TRUE)
-    }
-    else {
-      prob = prob + allNgrams[[s]]
+printFalseNegatives <- function(sample, ngrams) {
+  for(d in sample) {
+    if(! isValidDns(toString(d), ngrams)) {
+      print(paste0("False-negative: ", d))
     }
   }
-  return(prob > 50)
 }
 
 
@@ -200,44 +201,56 @@ hist(testDns.validValues, breaks = 5000, col = "red", freq = FALSE, xlim = range
 # Testy właściwe #
 ##################
 
-testDns.truePositives = 0;
-testDns.trueNegatives = 0;
-testDns.falsePositives = 0;
-testDns.falseNegatives = 0;
-testDns.i = 0
-testDns.pb = txtProgressBar(min = 0, max = length(validDns.domains), initial = 0)
-print("Checking valid class...")
-for(d in validDns.domains) {
-  if(isValidDnsDist(toString(d), totalDns.ngrams)) {
-    testDns.truePositives = testDns.truePositives + 1
+testDnsBayes <- function() {
+  
+  testDns.truePositives = 0;
+  testDns.trueNegatives = 0;
+  testDns.falsePositives = 0;
+  testDns.falseNegatives = 0;
+  testDns.validSample = sample(validDns.domains, 10000)
+  testDns.evilSample = sample(c( evilDns.domainsHEX, evilDns.domainsB32 ), 1000)
+  
+  print("Checking valid class...")
+  testDns.i = 0
+  testDns.pb = txtProgressBar(min = 0, max = length(testDns.validSample), initial = 0)
+  for(d in testDns.validSample) {
+    if(isValidDns(toString(d), totalDns.ngrams)) {
+      testDns.truePositives = testDns.truePositives + 1
+    }
+    else {
+      testDns.falseNegatives = testDns.falseNegatives + 1
+      #print(paste0("False-negative: ", d))
+    }
+    testDns.i = testDns.i + 1
+    setTxtProgressBar(testDns.pb, testDns.i)
   }
-  else {
-    testDns.falseNegatives = testDns.falseNegatives + 1
-    print(paste0("False-positive: ", d))
+  close(testDns.pb)
+  testDns.truePositives = testDns.truePositives / length(testDns.validSample)
+  testDns.falseNegatives = testDns.falseNegatives / length(testDns.validSample)
+  
+  print("Checking evil class...")
+  testDns.i = 0
+  testDns.pb = txtProgressBar(min = 0, max = length(testDns.evilSample), initial = 0)
+  for(d in testDns.evilSample) {
+    if(! isValidDns(toString(d), totalDns.ngrams)) {
+      testDns.trueNegatives = testDns.trueNegatives + 1
+    }
+    else {
+      testDns.falsePositives = testDns.falsePositives + 1
+    }
+    testDns.i = testDns.i + 1
+    setTxtProgressBar(testDns.pb, testDns.i)
   }
-  testDns.i = testDns.i + 1
-  setTxtProgressBar(testDns.pb, testDns.i)
+  close(testDns.pb)
+  testDns.trueNegatives = testDns.trueNegatives / length(testDns.evilSample)
+  testDns.falsePositives = testDns.falsePositives / length(testDns.evilSample)
+  
+  print(paste0("True VALID-DNS rate:    ", round(10000 * testDns.truePositives)/100, "%" ))
+  print(paste0("True INVALID-DNS rate:  ", round(10000 * testDns.trueNegatives)/100, "%" ))
+  print(paste0("False VALID-DNS rate:   ", round(10000 * testDns.falsePositives)/100, "%" ))
+  print(paste0("False INVALID-DNS rate: ", round(10000 * testDns.falseNegatives)/100, "%" ))
+  
 }
-close(testDns.pb)
-testDns.i = 0
-testDns.pb = txtProgressBar(min = 0, max = length(evilDns.domains), initial = 0)
-print("Checking evil class...")
-for(d in evilDns.domains) {
-  if(! isValidDnsDist(toString(d), totalDns.ngrams)) {
-    testDns.trueNegatives = testDns.trueNegatives + 1
-  }
-  else {
-    testDns.falsePositives = testDns.falsePositives + 1
-  }
-  testDns.i = testDns.i + 1
-  setTxtProgressBar(testDns.pb, testDns.i)
-}
-
-print(paste0("True VALID-DNS rate:    ", round(10000 * testDns.truePositives / length(validDns.domains))/100, "%" ))
-print(paste0("True INVALID-DNS rate:  ", round(10000 * testDns.trueNegatives / length(evilDns.domains))/100, "%" ))
-print(paste0("False VALID-DNS rate:   ", round(10000 * testDns.falsePositives / length(evilDns.domains))/100, "%" ))
-print(paste0("False INVALID-DNS rate: ", round(10000 * testDns.falseNegatives / length(validDns.domains))/100, "%" ))
-
 
 
 
