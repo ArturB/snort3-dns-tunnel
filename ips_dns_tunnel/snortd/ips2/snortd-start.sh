ae8d8171a6ec2489e78fdbe96a1304f59954a27b
Artur M. Brodzki <artur@brodzki.org>
2019-05-02 22:35:09 +0200

    typo fix

--- ips_dns_tunnel/snortd/ips2/snortd-start.sh
+++ ips_dns_tunnel/snortd/ips2/snortd-start.sh
@@ -8,7 +8,7 @@ ethtool -K ens33 tso off gro off
 ethtool -K ens37 tso off gro off
 
 snort --daq afpacket -i "ens33:ens37" \
-      -Q --tweaks inline -s 65535 $@ & \
+      -Q --tweaks inline -s 65535 $@ \
       --plugin-path /usr/local/snort/lib64/snort_extra \ 
       -A alert_full \
-      -R ~/myriles.rules
+      -R ~/myriles.rules &
