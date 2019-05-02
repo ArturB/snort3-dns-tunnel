b0213bce66bf7ba374fbf035f52bf803831f85fa
Artur M. Brodzki <artur@brodzki.org>
2019-05-02 22:20:34 +0200

    reorganize

--- vagrant/mdns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~
-
-apt update
-
-# export colourful prompt
-if [[ -z `cat ~/.bashrc | grep "export PS1="` ]]; then
-    echo 'export PS1="\[\e[32m\][\u@\h:\w]$ \[\e[0m\]"' >> ~/.bashrc
-fi
