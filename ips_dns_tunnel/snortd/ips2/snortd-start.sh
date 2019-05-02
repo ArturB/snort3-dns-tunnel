a6b8811146db429cf83bee3fee53e507a45abe57
Artur M. Brodzki <artur@brodzki.org>
2019-05-02 22:30:23 +0200

    ips2 snortd-start.sh

--- ips_dns_tunnel/snortd/ips2/snortd-start.sh
+++ ips_dns_tunnel/snortd/ips2/snortd-start.sh
@@ -7,4 +7,8 @@ ip l set ens37 mtu 4000
 ethtool -K ens33 tso off gro off
 ethtool -K ens37 tso off gro off
 
-snort --daq afpacket -i "ens33:ens37" -Q --tweaks inline -A csv -s 65535 $@ &
+snort --daq afpacket -i "ens33:ens37" \
+      -Q --tweaks inline -s 65535 $@ & \
+      --plugin-path /usr/local/snort/lib64/snort_extra \ 
+      -A alert_full \
+      -R ~/myriles.rules
